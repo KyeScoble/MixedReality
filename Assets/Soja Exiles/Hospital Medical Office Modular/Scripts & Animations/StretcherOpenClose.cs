ed519e7ee5dfe29737f55f2f22e89ccce0160463
Kye Scoble <S3780157@student.rmit.edu.au>
2021-08-18 09:09:01 +1000

    Starting setup

---
+++ Assets/Soja Exiles/Hospital Medical Office Modular/Scripts & Animations/StretcherOpenClose.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StretcherOpenClose: MonoBehaviour {
+
+	public Animator Stretcher_collapse;
+	public bool open;
+	public Transform Player;
+
+	void Start (){
+		open = false;
+	}
+
+	void OnMouseOver (){
+		{
+			if (Player) {
+				float dist = Vector3.Distance (Player.position, transform.position);
+				if (dist < 15) {
+					if (open == false) {
+						if (Input.GetMouseButtonDown (0)) {
+							StartCoroutine (opening ());
+						}
+					} else {
+						if (open == true) {
+							if (Input.GetMouseButtonDown (0)) {
+								StartCoroutine (closing ());
+							}
+						}
+
+					}
+
+				}
+			}
+
+		}
+
+	}
+
+	IEnumerator opening(){
+		print ("you dropping it");
+		Stretcher_collapse.Play ("stretcherdrop");
+		open = true;
+		yield return new WaitForSeconds (.5f);
+	}
+
+	IEnumerator closing(){
+		print ("you are lifting it");
+		Stretcher_collapse.Play ("stretcherlift");
+		open = false;
+		yield return new WaitForSeconds (.5f);
+	}
+
+
+}
+
